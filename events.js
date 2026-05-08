fc8340c704b2f6eb6a2f34ad749d0b717482404e
Ulli-Buehler <ulli.buehler@gmx.de>
2026-05-08 08:18:52 +0200

    events.js aktualisieren

--- events.js
+++ events.js
@@ -1,1 +1,18 @@
-    const EVENTS = [
+const EVENTS = [
+  {
+    "title": "Konzert: Katakombenfest",
+    "city": "Tübingen",
+    "venue": "Sudhaus (Peripherie)",
+    "street": "",
+    "zip": "",
+    "date": "Sa, 16.05 · 20:00 Uhr",
+    "description": "Matti Klein Soul Trio, ab 18,00 €",
+    "image": "",
+    "url": "https://www.wasgehtapp.de/",
+    "source": "https://www.wasgehtapp.de/",
+    "id": "event-4",
+    "address": "Sudhaus (Peripherie), Tübingen, Deutschland",
+    "lat": null,
+    "lng": null
+  }
+];
